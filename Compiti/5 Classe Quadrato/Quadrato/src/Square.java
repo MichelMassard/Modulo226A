40e65a632fe0c270abc0bfde9a9b054b6eaaaae2
MichelMassard <michel.massard@samtrevano.ch>
2025-02-24 11:14:02 +0100

    Update Square.java

--- Compiti/5 Classe Quadrato/Quadrato/src/Square.java
+++ Compiti/5 Classe Quadrato/Quadrato/src/Square.java
@@ -1,3 +1,20 @@
 public class Square {
-    
+    int side;
+    public Square(int side) {
+        if (side > 0) {
+            this.side = side;
+        }
+    }
+    public Square(){
+        this.side = 0;
+    }
+
+    public int getSide() {
+        return side;
+    }
+
+    public void setSide(int side) {
+        this.side = side;
+    }
+
 }
